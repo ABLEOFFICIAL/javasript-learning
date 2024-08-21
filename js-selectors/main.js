9f862a37494b4bfdf47ab923f6eed07a197352d5
ABLEOFFICIAL <ekwesijoshua@gmail.com>
2024-08-21 21:19:50 +0200

    append items

--- js-selectors/main.js
+++ js-selectors/main.js
@@ -11,6 +11,7 @@ f.addEventListener('submit', display);
 function display(e){
     e.preventDefault();
 
+    // craete new div
     var newdiv = document.createElement('div')
 
     // add class to div
@@ -19,13 +20,33 @@ function display(e){
     // create text for newdiv
     var newtext = document.createTextNode(input.value);
 
+    // add newtext to newdid
     newdiv.appendChild(newtext);
 
+    // add newdiv to listitem
     additem.appendChild(newdiv);
+
+
+    // create a button
+    var delbtn = document.createElement('button');
+
+    // add class to delbtn
+    delbtn.className = 'select'
+
+    // create x btn
+    var xbtn = document.createTextNode('x')
+
+    // append x to delbtn
+    delbtn.appendChild(xbtn);
+
+    // append delbtn to newdiv
+    newdiv.appendChild(delbtn);
+
+    console.log(delbtn)
 
     // console.log(newdiv);
 
     // additem.appendChild()
-    console.log(input.value);
+    // console.log(input.value);
 }
 
