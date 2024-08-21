7fdee29f8e2c5b1423adbf27fec996db0f1fda05
ABLEOFFICIAL <ekwesijoshua@gmail.com>
2024-08-21 12:14:12 +0200

    append child

--- js-selectors/main.js
+++ js-selectors/main.js
@@ -1,4 +1,31 @@
-var listItem = document.querySelector('.bottom');
+var f = document.getElementById('form');
+var input = document.getElementById('fill');
+var additem = document.getElementById('btm');
 
-// console.log (listItem)
-// listItem.lastElementChild.Style.borderBottom = 1px solid gray;
+
+
+// add event
+f.addEventListener('submit', display);
+
+// create function
+function display(e){
+    e.preventDefault();
+
+    var newdiv = document.createElement('div')
+
+    // add class to div
+    newdiv.className = 'item-listing';
+
+    // create text for newdiv
+    var newtext = document.createTextNode(input.value);
+
+    newdiv.appendChild(newtext);
+
+    additem.appendChild(newdiv);
+
+    // console.log(newdiv);
+
+    // additem.appendChild()
+    console.log(input.value);
+}
+
